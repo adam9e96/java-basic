945f85b8d8f1108a2a6334d01bb06468ff37b05c
Fpkm9999 <fpkm99999@gmail.com>
2024-02-06 21:53:32 +0900

    2024-0206 code review

--- src/chapter7/Array_02.java
+++ src/chapter7/Array_02.java
@@ -3,69 +3,34 @@ package chapter7;
 /**
  * ===========================================================
  * fileName       : Array_02
- * date           : 2024-02-04
- * description    : 배열연습_02
+ * date           : 2024-02-06
+ * description    : 배열을 생성하고 초기화 하는 방법
  * ===========================================================
  */
 public class Array_02 {
-	public static void main(String[] args) {
-		/*
-		 * 학생 5명의 성적을 가지고 총점과 평균을 구함 
-		 * case1. 배열을 사용하지 않는 경우
-		 */
-		int score_001 = 95;
-		int score_002 = 76;
-		int score_003 = 67;
-		int score_004 = 56;
-		int score_005 = 87;
+    public static void main(String[] args) {
+        // case1. 직접 값을 넣어 초기화
+        int[] numbers = {1, 2, 3, 4, 5};
 
-		int total = score_001 + score_002 + score_003 + score_004 + score_005; // 학생 5명 성적의 총합
-		double avg = (double) total / 5; // 학생 5명 성적의 평균
-		System.out.println("총점은 " + total + "점이고, 평균은 " + avg + "입니다.");	// 출력
+        // case2. 배열의 크기만 지정하고 값을 나중에 할당
+        int[] numbers2 = new int[5];
+        numbers2[0] = 1;
+        numbers2[1] = 2;
+        numbers2[2] = 3;
+        numbers2[3] = 4;
+        numbers2[4] = 5;
 
-		// 문제점.
-		// 변수(학생)이(가) 추가될 때마다 변수를 추가로 선언하고 total 변수에 더해야 하며 avg 변수 계산도 수정해줘야 한다.
-		
-		// case2. 배열을 사용하는 경우
-		//
-		// 총점은 for 문을 이용해서 구할 수 있고, 배열의 갯수가 늘어난다 해도
-		// 총점을 구하는 for문과 평균을 구하는 코드를 수정할 필요가 없음
+        // case3. 중괄호를 사용한 초기화
+        int[] numbers3 = new int[]{1, 2, 3, 4, 5};
 
-		total = 0;
+        // case3. 반복문을 통한 초기화
+        int size = 5;
+        int[] numbers4 = new int[size];
+        for (int i = 0; i < size; i++) {
+            numbers4[i] = i + 1;
+        }
 
-		int[] scores = { 95, 76, 67, 56, 87 }; // 배열 생성 및 초기화 // 학생 성적 추가가 필요한 경우 여기만 수정하면 됨
 
-//		System.out.println(scores.length); 배열의 크기 출력
-		for (int i = 0; i < scores.length; i++) { // scores.length -> 배열의 갯수 // 현재 크기는 5
-			total += scores[i];
-		}
-		avg = (double) total / scores.length;	// 학생 성적 평균
-		System.out.println("총점은 " + total + "점이고, 평균은 " + avg + "입니다.");
 
-		// 가독성
-		total = 0; // 원래라면 new int[5]; 을 해야하는데 개발자가 만들고 일일히 초기화 하는게 귀찮으니
-		// 바로 배열생성할 때 초기화를 할거면 `new`키워드는 알아서 적용 되도록 했다.
-
-		/*
-		 * for문의 실무 주 사용 용도가 반복문.
-		 * 그 중에서도 배열에서 대부분 사용한다.
-		 */
-		/*
-		 * - 배열
-		 * 같은 타입의 여러 변수를 하나의 묶음으로 다루는 것이다.
-		 *
-		 * - 배열이 필요한 이유.
-		 * 1. 배열을 사용하면 변수의 개수를 줄일 수 있다.
-		 * 2. 배열을 사용하면 반복문과 함께 효율적인 연산을 수행할 수 있다.
-		 * 3. 배열을 사용하면 다차원의 데이터를 표현 할 수 있다.
-		 */
-	
-		/*
-		 * - 자바 배열의 특징
-		 * 1. 동일한 자료형만 저장
-		 * 2. 변수 선언시에 저장할 데이터의 개수를 지정
-		 * 3. 배열은 0번 부터 시작함
-		 * 4. 인덱스를 활용 할 수 있다.
-		 */
-	}
+    }
 }
