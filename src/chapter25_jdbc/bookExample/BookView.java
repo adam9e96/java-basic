fa0057b991089fddc6e690687b56ca048f11370b
Fpkm9999 <fpkm99999@gmail.com>
2024-02-25 12:51:46 +0900

    2024-02-24 JDBC 연습

--- src/chapter25_jdbc/bookExample/BookView.java
+++ src/chapter25_jdbc/bookExample/BookView.java
@@ -35,6 +35,11 @@ public class BookView {
                 case 5:
 //                    manaverprintAll();
                     break;
+                case 6:
+                    System.out.println("종료합니다.");
+                    manager.close();
+                    scanner.close();
+                    return;
                 default:
                     System.out.println("잚좃누르셨습니다. \n다시 선택해주세요.");
                     break;
@@ -43,6 +48,13 @@ public class BookView {
     }
 
     public static void printMenu() {
-
+        System.out.println("============Menu============");
+        System.out.println("1. 책 등록");
+        System.out.println("2. 책 검색");
+        System.out.println("3. 책 대여");
+        System.out.println("4. 책 반납");
+        System.out.println("5. 책 전체출력");
+        System.out.println("6. 종료");
+        System.out.println();
     }
 }
